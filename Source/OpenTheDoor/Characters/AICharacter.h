aec05f64f03fcff8958374170407392e164d1e52
yadyginid <48806320+yadyginid@users.noreply.github.com>
2026-01-21 17:42:54 +0300

    delete unused

---
+++ Source/OpenTheDoor/Characters/AICharacter.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ABaseCharacter.h"
+#include "AICharacter.generated.h"
+
+UCLASS()
+class OPENTHEDOOR_API AAICharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
